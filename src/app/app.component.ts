a7735280055e635328c291abb186138fcc65fab3
Viswa184 <viswanadhbabunuru@gmail.com>
2023-08-28 15:56:01 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit, AfterViewChecked {
   title = 'aReactiveform';
+  myForm: FormGroup;
+    angular="";
+    interval : any;
+
+
+    constructor(private fb: FormBuilder) {
+      this.myForm = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      });
+    }
+  
+    onSubmit() {
+      if (this.myForm.valid) {
+        const formData = this.myForm.value;
+        console.log(formData);
+      } else {
+        alert('Please enter proper details')
+      }
+    }
+  
+    ngOnChanges(): void {
+  
+      console.log('ngOnChanges function is called');
+    }
+
+    ngOnInit(): void {
+
+      console.log('ngOnInit function is called');
+      this.angular="parent component";//When the angular component is intiliazes onit() will be intiloazed
+  
+    }
+
+    ngAfterViewInit(): void {
+  
+      console.log('ngAfterViewInit function is called');
+  
+    }
+    ngAfterViewChecked(): void {
+  
+      console.log('ngAfterViewChecked function is called');
+  
+    }
+
+    ngOnDestroy(): void {
+  
+      clearInterval(this.interval);
+      console.log('ngOnDestroy function is called');
+  
+    }
+  
+    ngDoCheck():void
+    {
+  
+      console.log("ngDocheck is called");
+  
+    }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule,} from  '@angular/material/button';
+import {MatToolbarModule} from  '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,MatToolbarModule
   ],
+  exports: [MatButtonModule,MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
